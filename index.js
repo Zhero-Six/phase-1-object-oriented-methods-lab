5b6f998b8c3c704d7f98b0142e9a3d2bba089a75
Zhero-Six <vmochoge@gmail.com>
2025-04-02 20:52:19 +0300

    updates index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+
+function BoardMember(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+
+    this.veto = function() {
+        return "No, I must disagree";
+    };
+
+    this.approve = function() {
+        return "You can do that!";
+    };
+
+    this.doCharity = function() {
+        return "I like to help people.";
+    };
+
+    this.releasePressStatement = function() {
+        return "You will see great things from Scuber.";
+    };
+
+    this.sayHi = function() {
+        return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+    };
+}
+
+// Export for testing
+if (typeof module !== 'undefined') {
+    module.exports = BoardMember;
+}
